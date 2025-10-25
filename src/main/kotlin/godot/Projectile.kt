3d7add4d1878bd537d862a523b892f3d5ce680f6
christophergiron <cgarciag59@miumg.edu.gt>
2025-10-24 20:24:57 -0600

    feat: llaves porteadas a escenas
    se convirtio de nodo a escena las llaves

--- src/main/kotlin/godot/Projectile.kt
+++ src/main/kotlin/godot/Projectile.kt
@@ -4,27 +4,43 @@ import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
 import godot.api.AnimationPlayer
 import godot.api.Area2D
+import godot.api.Node
+import godot.api.Node2D
+import godot.core.StringName
 import godot.core.Vector2
 import godot.global.GD
 
 @RegisterClass
 class Projectile : Area2D() {
 
-	var velocidad = 400.0 // velocidad de movimiento
-	private var animationPlayer: AnimationPlayer? = null
+	private var speed = 600.0
+	private var direction = Vector2(-1.0, 0.0)
 
 	@RegisterFunction
-	override fun _process(delta: Double) {
-		// Mover el proyectil hacia la izquierda
-		position += Vector2(-velocidad * delta, 0.0)
-		animationPlayer = getNodeOrNull("AnimationPlayer") as? AnimationPlayer
-		animationPlayer?.play("Llave")
+	override fun _ready() {
+	}
 
-		// Eliminar si sale de la pantalla
-		if (position.x < -100.0) {
-			GD.print("Proyectil eliminado fuera de pantalla")
-			queueFree()
+	@RegisterFunction
+	override fun _physicsProcess(delta: Double) {
+		position += direction * speed * delta
+	}
+
+	@RegisterFunction
+	fun configurar(vel: Double, dir: Vector2) {
+		speed = vel
+		direction = dir.normalized()
+	}
+
+	@RegisterFunction
+	fun _on_area_2d_body_entered(body: Node) {
+		if (body.isInGroup("Player")) {
+			GD.print("Cucca fue alcanzada — GAME OVER")
+			val cucca = body as? Node
+			val dieMethod = StringName("die")
+			if (cucca != null && cucca.hasMethod(dieMethod)) {
+				cucca.call(dieMethod)
+				GD.print("Método 'die()' ejecutado correctamente.")
+			}
 		}
 	}
 }
-
